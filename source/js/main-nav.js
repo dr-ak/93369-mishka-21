e20a3c7a1de7f64747b52b7702a265dab17b49e9
dr-ak <alex-r-1982@mail.ru>
2020-12-02 00:40:36 +0700

    Адаптивная сетка для главной страницы (Доработка сетки)

--- source/js/main-nav.js
+++ source/js/main-nav.js
@@ -1,30 +1,14 @@
 let navMain = document.querySelector('.main-nav');
 let navToggle = document.querySelector('.main-nav__toggle');
-let siteListMobileItems = document.querySelectorAll('.site-list__item--mobile');
-
-let removeMobileItems = function () {
-  siteListMobileItems.forEach(function(item) {
-    item.classList.remove('site-list__item--mobile');
-  });
-}
-
-let addMobileItems = function () {
-  siteListMobileItems.forEach(function(item) {
-    item.classList.add('site-list__item--mobile');
-  });
-}
 
 navMain.classList.remove('main-nav--nojs');
-removeMobileItems();
 
 navToggle.addEventListener('click', function() {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('main-nav--opend')) {
+    navMain.classList.remove('main-nav--opend');
     addMobileItems();
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav--opend');
     removeMobileItems();
   }
 });
